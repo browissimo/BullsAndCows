c6af75e366c1b8f793133b1ca0e3e8b13e27448a
browissimo <browissimo@gmail.com>
2022-03-24 10:07:02 +0300

    Добавьте файлы проекта.

---
+++ BullsAndCows/inputParce.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullsAndCows
+{
+    internal class InputParce
+    {
+        public static bool TryParce(string input)
+        {
+            const string message = "Введите 4-значное число, состоящее из неповторяющихся цифр.";
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                return false;
+            }
+
+            if (input.Length != 4)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                return false;
+            }
+
+            var inputToArr = input.ToCharArray();
+
+            foreach (var c in inputToArr)
+            {
+                if (!char.IsDigit(c))
+                {
+                    Console.Clear();
+                    Console.WriteLine(message);
+                    return false;
+                }
+            }
+
+            var disc = input.Distinct();
+
+            if (disc.ToArray().Length != 4)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                return false; 
+            }
+
+            return true;
+        }
+    }
+}
---
+++ BullsAndCows/numberGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullsAndCows
+{
+    internal class NumberGenerator
+    {
+        public static int[] Generate()
+        {
+            var random = new Random();
+            int[] array = new int[4];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                bool qnic;
+                do
+                {
+                    array[i] = random.Next(0, 9);
+                    qnic = true;
+                    for (int j = 0; j < i; ++j)
+                        if (array[i] == array[j])
+                        {
+                            qnic = false;
+                            break;
+                        }
+                } while (!qnic);
+            }
+
+            return array;
+        }
+    }
+}
